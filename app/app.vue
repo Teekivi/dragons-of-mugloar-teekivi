f7756317756e1831170a4966ef6376be898c5b5f
Magnus Teekivi <magnus.teekivi@gmail.com>
2026-06-24 15:41:06 +0300

    Add a message next to "New Game" if game over

--- app/app.vue
+++ app/app.vue
@@ -21,7 +21,12 @@ watch(isGameOver, (newValue) => {
     class="mx-auto my-8 flex max-w-[800px] flex-col items-start gap-4 rounded-xl bg-amber-900 p-4"
   >
     <h1 class="font-cinzel text-xl text-amber-300">Dragons of Mugloar</h1>
-    <Button :disabled="isLoading" @click="startGame">New Game</Button>
+    <div class="flex items-center gap-2">
+      <Button :disabled="isLoading" @click="startGame">New Game</Button>
+      <div v-if="isGameOver" class="font-crimson text-white">
+        Game Over. Start a new one?
+      </div>
+    </div>
     <StateDisplay />
     <TasksAndShopTabs />
     <GameOverOverlay
