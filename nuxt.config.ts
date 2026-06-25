9067860912740a6c9787676c8561d1d39f975afb
Magnus Teekivi <magnus.teekivi@gmail.com>
2026-06-25 09:22:10 +0300

    Keep just the favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  app: {
-    head: {
-      link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
-    },
-  },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/google-fonts'],
   googleFonts: {
     families: {
