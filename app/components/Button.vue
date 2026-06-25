4f082fa11568b806460e333579f46350b3d93faf
Magnus Teekivi <magnus.teekivi@gmail.com>
2026-06-25 11:52:46 +0300

    Fix Button: horizontal center align

--- app/components/Button.vue
+++ app/components/Button.vue
@@ -9,7 +9,7 @@ defineProps<{
   <button
     :disabled="disabled"
     :class="[
-      'inline-flex items-center gap-1 rounded-md bg-amber-700 font-crimson text-white',
+      'inline-flex items-center justify-center gap-1 rounded-md bg-amber-700 font-crimson text-white',
       disabled ? 'opacity-50' : 'hover:bg-amber-600 active:bg-amber-500',
       small ? 'p-1 text-sm' : 'p-2',
     ]"
